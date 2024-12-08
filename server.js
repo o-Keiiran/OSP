69781128e28dbb9e094f07d23b506b0baeebf69f
o-Keiiran <keiranchetwynd@gmail.com>
2024-12-08 19:28:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,24 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
+  console.log('A user connected');
 
   socket.on('chat message', (msg) => {
+    console.log('Received message:', msg);
     io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log('User disconnected');
   });
 });
 
-app.use(express.static('public'));
-
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
